49ae5e99f4e4a84de72fcf4f359d8390ee99891d
Charlie Savage <cfis@zerista.com>
2015-01-31 16:25:53 -0700

    Fix database class, was incorrectly copy of the table class.

--- pkg/pgdiff-1.0.0/lib/database.rb
+++ pkg/pgdiff-1.0.0/lib/database.rb
@@ -1,90 +1,112 @@
 module PgDiff
-  class Table
-    attr_accessor :table_name, :schema, :attributes, :constraints, :indexes
+  class Database
+    attr_accessor :tables, :views, :sequences, :schemas, :domains, :rules, :functions, :triggers
 
-    def initialize(conn, schema, table_name)
-      @schema = schema
-      @table_name = table_name
-      @attributes = {}
-      @constraints = {}
-      @indexes = {}
-      @atlist = []
-
-      att_query = <<-EOT
-        select attname, format_type(atttypid, atttypmod) as a_type, attnotnull,  pg_get_expr(adbin, attrelid) as a_default
-        from pg_attribute left join pg_attrdef  on (adrelid = attrelid and adnum = attnum)
-        where attrelid = '#{schema}.#{table_name}'::regclass and not attisdropped and attnum > 0
-        order by attnum
+    def initialize(conn)
+      cls_query = <<-EOT
+        SELECT n.nspname, c.relname, c.relkind
+        FROM pg_catalog.pg_class c
+        LEFT JOIN pg_catalog.pg_user u ON u.usesysid = c.relowner
+        LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+        WHERE c.relkind IN ('r','S','v')
+        AND n.nspname NOT IN ('pg_catalog', 'pg_toast', 'information_schema')
+        ORDER BY 1,2;
       EOT
-      conn.query(att_query).each do |tuple|
-        attname = tuple['attname']
-        typedef = tuple['a_type']
-        notnull = tuple['attnotnull']
-        default = tuple['a_default']
-        @attributes[attname] = Attribute.new(attname, typedef, notnull, default)
-        @atlist << attname
+      @views = {}
+      @tables = {}
+      @sequences = {}
+      @schemas = {}
+      @domains = {}
+      @functions = {}
+      @rules = {}
+      @triggers = {}
+
+      conn.query(cls_query).each do |tuple|
+        schema = tuple['nspname']
+        relname = tuple['relname']
+        relkind = tuple['relkind']
+        case relkind
+          when 'r'
+            @tables["#{schema}.#{relname}"] = Table.new(conn, schema, relname)
+          when 'v'
+            @views ["#{schema}.#{relname}"] = View.new(conn, schema, relname)
+          when 'S'
+            @sequences["#{schema}.#{relname}"] = Sequence.new(conn, schema, relname)
+        end
       end
 
-      ind_query = <<-EOT
-        select indexrelid::regclass as indname, pg_get_indexdef(indexrelid) as def
-        from pg_index where indrelid = '#{schema}.#{table_name}'::regclass and not indisprimary
+      domain_qry = <<-EOT
+      SELECT n.nspname, t.typname,  pg_catalog.format_type(t.typbasetype, t.typtypmod) || ' ' ||
+         CASE WHEN t.typnotnull AND t.typdefault IS NOT NULL THEN 'not null default '||t.typdefault
+              WHEN t.typnotnull AND t.typdefault IS NULL THEN 'not null'
+              WHEN NOT t.typnotnull AND t.typdefault IS NOT NULL THEN 'default '|| t.typdefault
+              ELSE ''
+         END AS def
+      FROM pg_catalog.pg_type t
+         LEFT JOIN pg_catalog.pg_namespace n ON n.oid = t.typnamespace
+      WHERE t.typtype = 'd'
+      ORDER BY 1, 2
       EOT
-      conn.query(ind_query).each do |tuple|
-        name = tuple['indname']
+      conn.query(domain_qry).each do |tuple|
+        schema = tuple['nspname']
+        typename = tuple['typname']
         value = tuple['def']
-        @indexes[name] = value
+        @domains["#{schema}.#{typename}"] = value
       end
 
-      cons_query = <<-EOT
-        select conname, pg_get_constraintdef(oid) from pg_constraint where conrelid = '#{schema}.#{table_name}'::regclass
+      schema_qry = <<-EOT
+        select nspname from pg_namespace
       EOT
-      conn.query(cons_query).each do |tuple|
-        name = tuple['conname']
-        value = tuple['pg_get_constraintdef']
-        @constraints[name] = value
+      conn.query(schema_qry).each do |tuple|
+        schema = tuple['nspname']
+        @schemas[schema] = schema
       end
-      @constraints.keys.each do |cname|
-        @indexes.delete("#{schema}.#{cname}") if has_index?(cname)
+
+      func_query = <<-EOT
+       SELECT proname AS function_name
+       , nspname AS namespace
+       , lanname AS language_name
+       , pg_catalog.obj_description(pg_proc.oid, 'pg_proc') AS comment
+       , proargtypes AS function_args
+       , proargnames AS function_arg_names
+       , prosrc AS source_code
+       , proretset AS returns_set
+       , prorettype AS return_type,
+       provolatile, proisstrict, prosecdef
+       FROM pg_catalog.pg_proc
+       JOIN pg_catalog.pg_language ON (pg_language.oid = prolang)
+       JOIN pg_catalog.pg_namespace ON (pronamespace = pg_namespace.oid)
+       JOIN pg_catalog.pg_type ON (prorettype = pg_type.oid)
+       WHERE pg_namespace.nspname !~ 'pg_catalog|information_schema'
+       AND proname != 'plpgsql_call_handler'
+       AND proname != 'plpgsql_validator'
+      EOT
+
+      conn.exec(func_query).each_with_index do |tuple, i|
+        func = Function.new(conn, tuple)
+        @functions[func.signature] = func
       end
-    end
 
-    def has_attribute?(name)
-      @attributes.has_key?(name)
-    end
-
-    def attribute_index(name)
-      @atlist.index(name)
-    end
-
-    def has_index?(name)
-      @indexes.has_key?(name) || @indexes.has_key?("#{schema}.#{name}")
-    end
-
-    def has_constraint?(name)
-      @constraints.has_key?(name)
-    end
-
-    def table_creation
-      out = ["CREATE TABLE #{name} ("]
-      stmt = []
-      @atlist.each do |attname|
-        stmt << @attributes[attname].definition
+      rule_query = <<-EOT
+      select  schemaname || '.' ||  tablename || '.' || rulename as rule_name,
+              schemaname || '.' ||  tablename as tab_name,
+        rulename, definition
+      from pg_rules
+      where schemaname !~ 'pg_catalog|information_schema'
+      EOT
+      conn.exec(rule_query).each do |tuple|
+        @rules[tuple['rule_name']] = Rule.new(tuple['tab_name'], tuple['rulename'], tuple['definition'])
       end
-      out << stmt.join(",\n")
-      out << ");"
-      out.join("\n")
-    end
 
-    def name
-      "#{schema}.#{table_name}"
-    end
-
-    def index_creation
-      out = []
-      @indexes.values.each do |c|
-        out << (c+";")
+      trigger_query =  <<-EOT
+      select nspname || '.' || relname as tgtable, tgname, pg_get_triggerdef(t.oid) as tg_def
+      from pg_trigger t join pg_class c ON (tgrelid = c.oid ) JOIN pg_namespace n ON (c.relnamespace = n.oid)
+      where not tgisinternal
+      and nspname !~ 'pg_catalog|information_schema'
+      EOT
+      conn.exec(trigger_query).each do |tuple|
+        @triggers[tuple['tgtable'] + "." + tuple['tgname']] = Trigger.new(tuple['tgtable'], tuple['tgname'], tuple['tg_def'])
       end
-      out.join("\n")
     end
   end
 end
