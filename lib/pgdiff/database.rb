d1ef6fe6a9ecd909f0afb081cd2675cb223a5f5e
Charlie Savage <charlie.savage@orbitalinsight.com>
2023-05-16 01:24:19 -0700

    Support ignore schemas and add progress info

--- lib/pgdiff/database.rb
+++ lib/pgdiff/database.rb
@@ -3,7 +3,8 @@ module PgDiff
     attr_accessor :tables, :views, :sequences, :schemas, :domains, :rules, :functions, :triggers
 
     def initialize(connection, ignore_schemas: [])
-      ignore_schemas += ['pg_catalog', 'pg_toast', 'information_schema']
+      puts "Analyzing #{connection}"
+      ignore_schemas += %w['pg_catalog' 'pg_toast' 'information_schema']
       cls_query = <<~EOT
         SELECT n.nspname, c.relname, c.relkind
         FROM pg_catalog.pg_class c
@@ -22,6 +23,7 @@ module PgDiff
       @rules = {}
       @triggers = {}
 
+      puts "Querying Catalog..."
       connection.query(cls_query).each do |tuple|
         schema = tuple['nspname']
         relname = tuple['relname']
@@ -37,6 +39,7 @@ module PgDiff
         end
       end
 
+      puts "Querying Domains..."
       domain_qry = <<~EOT
       SELECT n.nspname, t.typname,  pg_catalog.format_type(t.typbasetype, t.typtypmod) || ' ' ||
          CASE WHEN t.typnotnull AND t.typdefault IS NOT NULL THEN 'not null default '|| t.typdefault
@@ -47,20 +50,17 @@ module PgDiff
       FROM pg_catalog.pg_type t
          LEFT JOIN pg_catalog.pg_namespace n ON n.oid = t.typnamespace
       WHERE t.typtype = 'd'
-        AND 
+        AND n.nspname NOT IN (#{ignore_schemas.join(', ')})
       ORDER BY 1, 2
       EOT
       connection.query(domain_qry).each do |tuple|
         schema = tuple['nspname']
-        if ignore_schemas.include?(schema)
-          next
-        end
-
         typename = tuple['typname']
         value = tuple['def']
         @domains["#{schema}.#{typename}"] = value
       end
 
+      puts "Querying Schemas..."
       schema_qry = <<~EOT
         SELECT nspname
         FROM pg_namespace
@@ -71,24 +71,27 @@ module PgDiff
         @schemas[schema] = schema
       end
 
+      puts "Querying Functions..."
       func_query = <<~EOT
-       SELECT proname AS function_name
-       , nspname AS namespace
-       , lanname AS language_name
-       , pg_catalog.obj_description(pg_proc.oid, 'pg_proc') AS comment
-       , proargtypes AS function_args
-       , proargnames AS function_arg_names
-       , prosrc AS source_code
-       , proretset AS returns_set
-       , prorettype AS return_type,
-       provolatile, proisstrict, prosecdef
+       SELECT proname AS function_name,
+         nspname AS namespace,
+         lanname AS language_name,
+         pg_catalog.obj_description(pg_proc.oid, 'pg_proc') AS comment,
+         proargtypes AS function_args,
+         proargnames AS function_arg_names,
+         prosrc AS source_code,
+         proretset AS returns_set,
+         prorettype AS return_type,
+         provolatile,
+         proisstrict,
+         prosecdef
        FROM pg_catalog.pg_proc
        JOIN pg_catalog.pg_language ON (pg_language.oid = prolang)
        JOIN pg_catalog.pg_namespace ON (pronamespace = pg_namespace.oid)
        JOIN pg_catalog.pg_type ON (prorettype = pg_type.oid)
-       WHERE pg_namespace.nspname !~ 'pg_catalog|information_schema'
-       AND proname != 'plpgsql_call_handler'
-       AND proname != 'plpgsql_validator'
+       WHERE pg_namespace.nspname NOT IN (#{ignore_schemas.join(', ')})
+         AND proname != 'plpgsql_call_handler'
+         AND proname != 'plpgsql_validator'
       EOT
 
       connection.exec(func_query).each_with_index do |tuple, i|
@@ -96,22 +99,26 @@ module PgDiff
         @functions[func.signature] = func
       end
 
+      puts "Querying Rules..."
       rule_query = <<~EOT
-      select  schemaname || '.' ||  tablename || '.' || rulename as rule_name,
-              schemaname || '.' ||  tablename as tab_name,
-        rulename, definition
-      from pg_rules
-      where schemaname !~ 'pg_catalog|information_schema'
+        SELECT  schemaname || '.' ||  tablename || '.' || rulename AS rule_name,
+                schemaname || '.' ||  tablename AS tab_name,
+          rulename, definition
+        FROM pg_rules
+        WHERE schemaname NOT IN (#{ignore_schemas.join(', ')})
       EOT
       connection.exec(rule_query).each do |tuple|
         @rules[tuple['rule_name']] = Rule.new(tuple['tab_name'], tuple['rulename'], tuple['definition'])
       end
 
+      puts "Querying Triggers..."
       trigger_query =  <<~EOT
-      select nspname || '.' || relname as tgtable, tgname, pg_get_triggerdef(t.oid) as tg_def
-      from pg_trigger t join pg_class c ON (tgrelid = c.oid ) JOIN pg_namespace n ON (c.relnamespace = n.oid)
-      where not tgisinternal
-      and nspname !~ 'pg_catalog|information_schema'
+        SELECT nspname || '.' || relname as tgtable, tgname, pg_get_triggerdef(pg_trigger.oid) as tg_def
+        FROM pg_trigger
+        JOIN pg_class ON pg_trigger.tgrelid = pg_class.oid
+        JOIN pg_namespace ON pg_class.relnamespace = pg_namespace.oid
+        WHERE NOT tgisinternal
+          AND nspname NOT IN (#{ignore_schemas.join(', ')})
       EOT
       connection.exec(trigger_query).each do |tuple|
         @triggers[tuple['tgtable'] + "." + tuple['tgname']] = Trigger.new(tuple['tgtable'], tuple['tgname'], tuple['tg_def'])
